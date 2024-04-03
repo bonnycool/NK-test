f50d0d8ce675eb9f6a86df43d323179c3ba4c4ed
bonnycool <bonnydavid543@gmail.com>
2024-04-03 23:08:42 +0530

    test phase 2

--- src/pages/SelectionPage.jsx
+++ src/pages/SelectionPage.jsx
@@ -14,7 +14,7 @@ function SelectionPage() {
         onClick={() => navigateTo("/onlinepage")}
         className="mx-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-20 px-20 rounded-lg text-2xl"
       >
-        Online Registration
+        Online Registrations
       </button>
       <button
         onClick={() => navigateTo("/spot-registration")}
